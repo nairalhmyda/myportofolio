595929b49776985c16c0275c6a835f7b7a551c99
nairalhmyda <naira.al@ui.ac.id>
2026-09-09 17:33:36 +0700

    Tutorial 2 Done

---
+++ main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Naira Al Humayda",
+        "npm": "2506532233",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa Ilmu Komputer Universitas Indonesia yang tertarik "
+            "pada Data Science."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Naira Al Humayda",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
